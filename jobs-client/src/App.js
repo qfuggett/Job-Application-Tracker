08afb8823cfe32c4b283685841a3b8d11f1c015f
QueenTesa Fuggett <qfuggett@umich.edu>
2021-02-04 02:47:50 -0500

    add connect and mapStateToProps

--- jobs-client/src/App.js
+++ jobs-client/src/App.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import './App.css';
+import { connect } from 'react-redux'
 
 class App extends Component {
 
   render(){
+
+    const jobsLis = this.props.jobs.map(jb => <li key={jb.id}>{jb.title} - {jb.applied ? "Applied" : "Did Not Apply"}</li>)
+
     return (
       <div className="App">
-        <h1>Jobs</h1>
+        <h1>Add Job</h1>
+        
+        <hr/>
+        <h1>My Jobs</h1>
+        <ul>
+          {this.props.loading? <h4>Loading . . . . . .</h4> : jobsLis}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+//allows you to access state through props
+const mapStateToProps = state => {
+  console.log("state testing", state)
+  return {
+    jobs: state.jobReducer.jobs,
+    loading: state.jobReducer.loading
+  }
+}
+
+export default connect(mapStateToProps)(App);
